364d094bd5bcc7be79693033d7943818ab0e5c2d
olaf oude reimer <orolaf@live.nl>
2023-06-09 14:37:32 +0100

    Added a way to set the flag back to 0 and comments

--- MagmaFlags/framework.h
+++ MagmaFlags/framework.h
@@ -5,7 +5,7 @@
 
 namespace MagmaEnum_32
 {
-	enum Flag : uint32_t; //Increase this value to uint64_t to double the amount of flags
+	enum Flag : uint32_t; 
 };
 
 class MagmaFlags_32
@@ -15,17 +15,26 @@ public:
 	void* operator new(size_t size) = delete;
 	void operator delete(void* pointer) = delete;
 
+	//Check if the flag is 1
 	inline bool operator==(const MagmaEnum_32::Flag flag) const
 	{
 		return m_flags & flag;
 	}
+	//Check if the flag is not 1
 	inline bool operator!=(const MagmaEnum_32::Flag flag) const
 	{
 		return !(m_flags & flag);
 	}
+	//Set the flag to 1
 	inline MagmaFlags_32& operator+=(const MagmaEnum_32::Flag flag) noexcept
 	{
 		m_flags = static_cast<MagmaEnum_32::Flag>(m_flags | flag);
+		return *this;
+	}
+	//Set the flag to 0
+	inline MagmaFlags_32& operator-=(const MagmaEnum_32::Flag flag) noexcept
+	{
+		m_flags = static_cast<MagmaEnum_32::Flag>(m_flags ^ flag);
 		return *this;
 	}
 	inline MagmaFlags_32& operator=(const MagmaFlags_32& magmaFlags) noexcept
@@ -36,7 +45,7 @@ public:
 		m_flags = magmaFlags.m_flags;
 		return *this;
 	}
-
+	//Reset all flags back to 0
 	void Clear()
 	{
 		m_flags = static_cast<MagmaEnum_32::Flag>(m_flags ^ m_flags);
@@ -45,7 +54,7 @@ public:
 
 namespace MagmaEnum_64
 {
-	enum Flag : uint64_t; //Increase this value to uint64_t to double the amount of flags
+	enum Flag : uint64_t; 
 };
 
 class MagmaFlags_64
@@ -54,18 +63,26 @@ class MagmaFlags_64
 public:
 	void* operator new(size_t size) = delete;
 	void operator delete(void* pointer) = delete;
-
+	//Check if the flag is 1
 	inline bool operator==(const MagmaEnum_64::Flag flag) const
 	{
 		return m_flags & flag;
 	}
+	//Check if the flag is not 1
 	inline bool operator!=(const MagmaEnum_64::Flag flag) const
 	{
 		return !(m_flags & flag);
 	}
+	//Set the flag to 1
 	inline MagmaFlags_64& operator+=(const MagmaEnum_64::Flag flag) noexcept
 	{
 		m_flags = static_cast<MagmaEnum_64::Flag>(m_flags | flag);
+		return *this;
+	}
+	//Set the flag to 0
+	inline MagmaFlags_64& operator-=(const MagmaEnum_64::Flag flag) noexcept
+	{
+		m_flags = static_cast<MagmaEnum_64::Flag>(m_flags ^ flag);
 		return *this;
 	}
 	inline MagmaFlags_64& operator=(const MagmaFlags_64& magmaFlags) noexcept
@@ -76,7 +93,7 @@ public:
 		m_flags = magmaFlags.m_flags;
 		return *this;
 	}
-
+	//Reset all flags back to 0
 	void Clear()
 	{
 		m_flags = static_cast<MagmaEnum_64::Flag>(m_flags ^ m_flags);
