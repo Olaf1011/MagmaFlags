73e00e812609becfa01882d5ae56d5f75d84db87
olaf oude reimer <orolaf@live.nl>
2023-06-09 14:37:08 +0100

    Added 64 bit enum to list

--- MagmaFlags/MagmaEnum.hpp
+++ MagmaFlags/MagmaEnum.hpp
@@ -3,7 +3,7 @@
 
 namespace MagmaEnum_32
 {
-	enum Flag : uint32_t //Increase this value to uint64_t to double the amount of flags
+	enum Flag : uint32_t 
 	{
 			RUNNING = 1u << 0 
 		,	AGGRESSIVE = 1u << 1
@@ -14,4 +14,8 @@ namespace MagmaEnum_32
 		,	EXAMPLE  = 1u << 6
 		,	MAX_VALUE_AT_U32 = 1u << 31 // Can still be used
 	};
+}
+namespace MagmaEnum_64
+{
+	enum Flag : uint64_t;
 }
